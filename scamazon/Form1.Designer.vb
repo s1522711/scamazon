792fece80bee3c749e4ff47b7a1e69c187a98eae
tamir <tamir@TAMIR-MAIN>
2023-06-11 23:25:27 +0300

    Add project files.

---
+++ scamazon/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        TextBox2 = New TextBox()
+        Button2 = New Button()
+        ProgressBar1 = New ProgressBar()
+        Timer1 = New Timer(components)
+        TextBox3 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Button3 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(12, 12)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(197, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(215, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(81, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "Add To Cart!"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(12, 56)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.ReadOnly = True
+        TextBox2.Size = New Size(197, 142)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(215, 34)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(81, 23)
+        Button2.TabIndex = 4
+        Button2.Text = "Order!"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Location = New Point(12, 12)
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(197, 23)
+        ProgressBar1.Step = 5
+        ProgressBar1.TabIndex = 5
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        Timer1.Interval = 2000
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(12, 76)
+        TextBox3.Multiline = True
+        TextBox3.Name = "TextBox3"
+        TextBox3.ReadOnly = True
+        TextBox3.Size = New Size(197, 142)
+        TextBox3.TabIndex = 3
+        TextBox3.Visible = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 38)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(59, 15)
+        Label1.TabIndex = 6
+        Label1.Text = "Your Cart:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(12, 38)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(75, 15)
+        Label2.TabIndex = 7
+        Label2.Text = "Order Status:"
+        Label2.Visible = False
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 53)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(162, 15)
+        Label3.TabIndex = 7
+        Label3.Text = "INSERT ACTUAL STATUS HERE"
+        Label3.Visible = False
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(215, 12)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(81, 23)
+        Button3.TabIndex = 8
+        Button3.Text = "New Order?"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(305, 227)
+        Controls.Add(Button3)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(ProgressBar1)
+        Controls.Add(Button2)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "Form1"
+        Text = "Scamzon Terminal"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button3 As Button
+End Class
---
+++ scamazon/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox2.Text = "" Then
+            TextBox2.Text = TextBox2.Text + TextBox1.Text
+        ElseIf TextBox1.Text IsNot "" Then
+            TextBox2.Text = TextBox2.Text + vbNewLine + TextBox1.Text
+        End If
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ProgressBar1.Value = 0
+        Timer1.Start()
+        TextBox3.Text = "Your Order:" + vbNewLine + TextBox2.Text
+        TextBox2.Text = ""
+        TextBox3.Visible = True
+        ProgressBar1.Visible = True
+        Label2.Visible = True
+        Label3.Visible = True
+        Button2.Enabled = False
+        TextBox1.Visible = False
+        Button1.Visible = False
+        TextBox2.Visible = False
+        Label1.Visible = False
+        Label3.Text = "Connecting To Scamazon!"
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If ProgressBar1.Value < 100 Then
+            ProgressBar1.PerformStep()
+            If ProgressBar1.Value = 20 Then
+                Label3.Text = "Connecting To Payment Proccesor!"
+            End If
+            If ProgressBar1.Value = 50 Then
+                Label3.Text = "Paying For The Order!"
+            End If
+            If ProgressBar1.Value = 80 Then
+                Label3.Text = "Draining Your Bank Account!"
+            End If
+            If ProgressBar1.Value = 90 Then
+                Label3.Text = "Sending Your Order!"
+            End If
+        Else
+            Timer1.Stop()
+            Label3.Text = "Order Delivered!"
+            Button3.Visible = True
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Stop()
+        Button2.Enabled = True
+        TextBox3.Visible = False
+        ProgressBar1.Visible = False
+        Label2.Visible = False
+        Label3.Visible = False
+        TextBox1.Visible = True
+        Button1.Visible = True
+        TextBox2.Visible = True
+        Label1.Visible = True
+        Button3.Visible = False
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Button2.Enabled = True
+        TextBox3.Visible = False
+        ProgressBar1.Visible = False
+        Label2.Visible = False
+        Label3.Visible = False
+        TextBox1.Visible = True
+        Button1.Visible = True
+        TextBox2.Visible = True
+        Label1.Visible = True
+        Button3.Visible = False
+    End Sub
+End Class
